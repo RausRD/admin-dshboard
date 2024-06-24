38565db52d46e8f2c68c85f48fa8b17b552b5a85
Ruslan Dunets <dunecruslan2@gmail.com>
2024-06-24 15:37:50 +0300

    add AddPage

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,8 +1,65 @@
 import React from 'react'
+import styles from '@/app/ui/dashboard/products/products.module.css'
+import Search from '@/app/ui/dashboard/search/search'
+import Link from 'next/link'
+import Image from 'next/image'
+import Pagination from '@/app/ui/dashboard/pagination/pagination'
 
 const ProductsPage = () => {
 	return (
-		<div>ProductsPage</div>
+		<div className={styles.container}>
+			<div className={styles.top}>
+				<Search placeholder='Search for a product' />
+				<Link href='/dashboard/products/add'>
+					<button className={styles.addBtn}>Add New</button>
+				</Link>
+			</div>
+			<table className={styles.table}>
+				<thead>
+					<tr>
+						<td>Title</td>
+						<td>Description</td>
+						<td>Price</td>
+						<td>Created At</td>
+						<td>Stock</td>
+						<td>Action</td>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							<div className={styles.product}>
+								<Image
+									src='/noproduct.jpg'
+									alt=''
+									width={40}
+									height={40}
+									className={styles.productImg}
+								/>
+								Iphone
+							</div>
+						</td>
+						<td>Desc</td>
+						<td>$999</td>
+						<td>13.02.2022</td>
+						<td>72</td>
+						<td>
+							<div className={styles.btns}>
+								<Link href='/'>
+									<button className={`${styles.btn} ${styles.view}`}>
+										View
+									</button>
+								</Link>
+								<button className={`${styles.btn} ${styles.delete}`}>
+									Delete
+								</button>
+							</div>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<Pagination />
+		</div>
 	)
 }
 
--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,55 +1,65 @@
 import styles from '../../ui/dashboard/users/users.module.css'
-import Search from "@/app/ui/dashboard/search/search";
-import Link from "next/link";
-import Image from "next/image";
-import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import Search from '@/app/ui/dashboard/search/search'
+import Link from 'next/link'
+import Image from 'next/image'
+import Pagination from '@/app/ui/dashboard/pagination/pagination'
 
 const UsersPage = () => {
 	return (
-		 <div className={styles.container}>
-			 <div className={styles.top}>
-				 <Search placeholder='Search for a user'/>
-				 <Link href='/dashboard/users/add'>
-					 <button className={styles.addBtn}>Add New</button>
-				 </Link>
-			 </div>
-			 <table className={styles.table}>
-				 <thead>
-				 <tr>
-					 <td>Name</td>
-					 <td>Email</td>
-					 <td>Created At</td>
-					 <td>Role</td>
-					 <td>Status</td>
-					 <td>Action</td>
-				 </tr>
-				 </thead>
-				 <tbody>
-				 <tr>
-					 <td>
-						 <div className={styles.user}>
-							 <Image src='/noavatar.png' alt='' width={40} height={40} className={styles.userImg}/>
-							 Jonh doe
-						 </div>
-					 </td>
-					 <td>john@gmail.com</td>
-					 <td>12.01.2022</td>
-					 <td>Admin</td>
-					 <td>active</td>
-					 <td>
-						 <div className={styles.btns}>
-							 <Link href='/'>
-								 <button className={`${styles.btn} ${styles.view}`}>View</button>
-							 </Link>
-							 <button className={`${styles.btn} ${styles.delete}`}>Delete</button>
-						 </div>
-					 </td>
-				 </tr>
-				 </tbody>
-			 </table>
-			 <Pagination/>
-		 </div>
-	);
-};
+		<div className={styles.container}>
+			<div className={styles.top}>
+				<Search placeholder='Search for a user' />
+				<Link href='/dashboard/users/add'>
+					<button className={styles.addBtn}>Add New</button>
+				</Link>
+			</div>
+			<table className={styles.table}>
+				<thead>
+					<tr>
+						<td>Name</td>
+						<td>Email</td>
+						<td>Created At</td>
+						<td>Role</td>
+						<td>Status</td>
+						<td>Action</td>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							<div className={styles.user}>
+								<Image
+									src='/noavatar.png'
+									alt=''
+									width={40}
+									height={40}
+									className={styles.userImg}
+								/>
+								Jonh doe
+							</div>
+						</td>
+						<td>john@gmail.com</td>
+						<td>12.01.2022</td>
+						<td>Admin</td>
+						<td>active</td>
+						<td>
+							<div className={styles.btns}>
+								<Link href='/'>
+									<button className={`${styles.btn} ${styles.view}`}>
+										View
+									</button>
+								</Link>
+								<button className={`${styles.btn} ${styles.delete}`}>
+									Delete
+								</button>
+							</div>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<Pagination />
+		</div>
+	)
+}
 
-export default UsersPage;
+export default UsersPage
