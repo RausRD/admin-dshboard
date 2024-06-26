063a127b85680fa17f30bc4cf4fb259ea08e1446
Ruslan Dunets <dunecruslan2@gmail.com>
2024-06-26 11:19:28 +0300

    add prpduct pagination and functionality

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -4,8 +4,12 @@ import Search from '@/app/ui/dashboard/search/search'
 import Link from 'next/link'
 import Image from 'next/image'
 import Pagination from '@/app/ui/dashboard/pagination/pagination'
+import { fetchProducts } from '@/app/lib/data'
 
-const ProductsPage = () => {
+const ProductsPage = async ({searchParams}) => {
+	const q = searchParams?.q || ''
+	const page = searchParams?.page || 1
+	const { count, products } = await fetchProducts(q, page)
 	return (
 		<div className={styles.container}>
 			<div className={styles.top}>
@@ -26,39 +30,41 @@ const ProductsPage = () => {
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td>
-							<div className={styles.product}>
-								<Image
-									src='/noproduct.jpg'
-									alt=''
-									width={40}
-									height={40}
-									className={styles.productImg}
-								/>
-								Iphone
-							</div>
-						</td>
-						<td>Desc</td>
-						<td>$999</td>
-						<td>13.02.2022</td>
-						<td>72</td>
-						<td>
-							<div className={styles.btns}>
-								<Link href='/dashboard/products/test'>
-									<button className={`${styles.btn} ${styles.view}`}>
-										View
+					{products.map(product => (
+						<tr key={product.id}>
+							<td>
+								<div className={styles.product}>
+									<Image
+										src={product.img || '/noproduct.jpg'}
+										alt=''
+										width={40}
+										height={40}
+										className={styles.productImg}
+									/>
+									{product.title}
+								</div>
+							</td>
+							<td>{product.desc}</td>
+							<td>${product.price}</td>
+							<td>{product.createdAt?.toString().slice(4, 16)}</td>
+							<td>{product.stock}</td>
+							<td>
+								<div className={styles.btns}>
+									<Link href={`/dashboard/products/${product.id}`}>
+										<button className={`${styles.btn} ${styles.view}`}>
+											View
+										</button>
+									</Link>
+									<button className={`${styles.btn} ${styles.delete}`}>
+										Delete
 									</button>
-								</Link>
-								<button className={`${styles.btn} ${styles.delete}`}>
-									Delete
-								</button>
-							</div>
-						</td>
-					</tr>
+								</div>
+							</td>
+						</tr>
+					))}
 				</tbody>
 			</table>
-			<Pagination />
+			<Pagination count={count}/>
 		</div>
 	)
 }
